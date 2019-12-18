b1d700291d6f300d5c72f7c5fa97f624b9feed5f
Michael Wehrley <michaelwehrley@gmail.com>
2019-12-18 13:08:31 -0600

    question `addf`

--- test.js
+++ test.js
@@ -2,8 +2,33 @@ function log(arg) {
   document.writeln(arg)
 }
 
+function add(a, b) {
+  debugger
+  return a + b;
+}
+
+function sub(a, b) {
+  return a - b;
+}
+
+function mult(a, b) {
+  return a * b;
+}
+
 function identity(id) {
   return id;
 }
 
-log(identity(3));
+function identityf(x) {
+  return function() {
+    return x;
+  }
+}
+
+function addf(x) {
+  return function(y) {
+    return add(x, y)
+  }
+}
+
+log(addf(3)(4))
