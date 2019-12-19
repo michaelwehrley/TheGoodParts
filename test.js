785402fac725cf7737ff0ff9d0142539e5e11e87
Michael Wehrley <michaelwehrley@gmail.com>
2019-12-18 21:36:43 -0600

    notes

--- test.js
+++ test.js
@@ -1,3 +1,28 @@
+// Does not work with `new funcA.bind(thisArg, args)`
+if (Function.prototype.bind) (function () {
+  var slice = Array.prototype.slice;
+  Function.prototype.bind = function () {
+    var thatFunc = this, thatArg = arguments[0];
+    
+    // We have access to the `arguments`
+    // removes all arguments after 1 by removing `this` (or `null` in this case)
+    var args = slice.call(arguments, 1);
+    if (typeof thatFunc !== 'function') {
+      // closest thing possible to the ECMAScript 5
+      // internal IsCallable function
+      throw new TypeError('Function.prototype.bind - ' +
+        'what is trying to be bound is not callable');
+    }
+    return function () {
+      // We have access to the `arguments` of the function called
+      // add to the args array the NEW arguments - see no `(arguments, 1)`
+      var funcArgs = args.concat(slice.call(arguments))
+      debugger;
+      return thatFunc.apply(thatArg, funcArgs);
+    };
+  };
+})();
+
 function log(arg) {
   document.writeln(arg)
 }
@@ -26,8 +51,5 @@ function add(a, b) {
 
 function addf(x) {
   return add.bind(null, x)
-  // versus return add.bind(null, x, 4)
-  // log(addf(3)())
 }
-
-log(addf(3)(4)) // 7
+log(addf(3)(4, 5, 6)) // 7
