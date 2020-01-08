ae7e4450b9cf8e4f5687fbe25a6fff3aad11670e
Michael Wehrley <michaelwehrley@gmail.com>
2020-01-08 13:00:15 -0600

    liftf

--- test.js
+++ test.js
@@ -1,27 +1,29 @@
 // Does not work with `new funcA.bind(thisArg, args)`
-if (Function.prototype.bind) (function () {
-  var slice = Array.prototype.slice;
-  Function.prototype.bind = function () {
-    var thatFunc = this, thatArg = arguments[0];
+// if (Function.prototype.bind) (function () {
+//   var slice = Array.prototype.slice;
+//   Function.prototype.bind = function () {
+//     var thatFunc = this, thatArg = arguments[0];
     
-    // We have access to the `arguments`
-    // removes all arguments after 1 by removing `this` (or `null` in this case)
-    var args = slice.call(arguments, 1);
-    if (typeof thatFunc !== 'function') {
-      // closest thing possible to the ECMAScript 5
-      // internal IsCallable function
-      throw new TypeError('Function.prototype.bind - ' +
-        'what is trying to be bound is not callable');
-    }
-    return function () {
-      // We have access to the `arguments` of the function called
-      // add to the args array the NEW arguments - see no `(arguments, 1)`
-      var funcArgs = args.concat(slice.call(arguments))
-      debugger;
-      return thatFunc.apply(thatArg, funcArgs);
-    };
-  };
-})();
+//     // We have access to the `arguments`
+//     // removes all arguments after 1 by removing `this` (or `null` in this case)
+//     var args = slice.call(arguments, 1);
+//     if (typeof thatFunc !== 'function') {
+//       // closest thing possible to the ECMAScript 5
+//       // internal IsCallable function
+//       throw new TypeError('Function.prototype.bind - ' +
+//         'what is trying to be bound is not callable');
+//     }
+//     return function () {
+//       // We have access to the `arguments` of the function called
+//       // add to the args array the NEW arguments - see no `(arguments, 1)`
+//       var funcArgs = args.concat(slice.call(arguments))
+//       debugger;
+//       return thatFunc.apply(thatArg, funcArgs);
+//     };
+//   };
+// })();
+
+// log(addf(3)(4, 5, 6)) // 7
 
 function log(arg) {
   document.writeln(arg)
@@ -41,7 +43,7 @@ function sub(a, b) {
   return a - b;
 }
 
-function mult(a, b) {
+function mul(a, b) {
   return a * b;
 }
 
@@ -50,6 +52,19 @@ function add(a, b) {
 }
 
 function addf(x) {
-  return add.bind(null, x)
+  return function (y) {
+    return x + y;
+  }
 }
-log(addf(3)(4, 5, 6)) // 7
+
+function liftf(bfn) {
+  return function(x) {
+    return function(y) {
+      return bfn(x, y);
+    }
+  }
+}
+
+var addf = liftf(add);
+log(addf(3)(4)) // 7
+log(liftf(mul)(5)(6)) // 30
